40932a7397a9a9c8cbc05aa9d5045ad72061f655
tpriestley <tjpriestley@gmail.com>
2020-10-28 17:15:05 -0700

    Updates

---
+++ precache-manifest.ca05bee428d9535f4a2c7eb70fe43653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1157250696142f09a0512727b9838e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a54346829ebe028325da",
+    "url": "/static/css/main.457b9bc6.chunk.css"
+  },
+  {
+    "revision": "7f1f329458553489e71d",
+    "url": "/static/js/2.d0c2c92e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d0c2c92e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a54346829ebe028325da",
+    "url": "/static/js/main.e3571d3b.chunk.js"
+  },
+  {
+    "revision": "d1c6d7dfdaf371a90a39",
+    "url": "/static/js/runtime-main.73e97af3.js"
+  }
+]);
